4e8a5dc70f07573de5f7864b4e350b50febe0eaf
Sayyed <55197683+Sayyed-G@users.noreply.github.com>
2020-12-23 17:33:03 -0500

    Actualizado

--- index.php
+++ index.php
@@ -25,6 +25,50 @@ $request = $_SERVER['QUERY_STRING'];
         case "guardar-usuario":
             include_once "App/vistas/uCrear.php";
             break;
+        case "validar":
+            $codigo = $_POST["codigo"];
+            $controladorUsuario = new ControladorUsuario();
+            $controladorUsuario-> validar($codigo);
+            break;
+        case "api/estudiantes":
+          echo  $metodo = $_SERVER["REQUEST_METHOD"];
+            $controladorUsuario = new ControladorUsuario();
+        if ($metodo == "GET") {
+
+            $resultados = $controladorUsuario->mostrarEstudiantes();
+            $estudiantes[] = null;
+
+            foreach ($resultados as $key => $estudiante) {
+                $estudiantes[$key] = array(
+                    "nombres" => $estudiante["nombres"],
+                    "apellidos" => $estudiante["apellidos"]
+                );
+            }
+            echo json_encode($estudiantes);
+        }
+            if ($metodo == "POST"){
+                $nombres = $_POST["nombres"];
+                $apellidos= $_POST["apellidos"];
+                $codigo = (int)$_POST["codigo"];
+                $password = $_POST["password"];
+                $tipo = $_POST["tipo"];
+               $resultado = $controladorUsuario->crearUsuario($nombres,$apellidos,$codigo, $password, $tipo);
+                if ($resultado == "Guardado"){
+                    echo json_encode(array(
+                        "codigo"=>"200",
+                        "status"=>"error"
+                    ));
+                }else{
+                    echo json_encode(array(
+                        "codigo" => "500",
+                        "status" => "error"
+                    ));
+                }
+               //crear estudiantes
+            }
+
+            break;
+
         default:
             include_once "App/vistas/uLogin.php";
             break;
